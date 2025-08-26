4349fdedd35be9660098f78af8227661d2a0792f
Elias Gideon <30030348+enajx@users.noreply.github.com>
2025-08-26 16:54:56 +0200

    evaluate sensitivty of blastocyst parameters

--- src/models/Blastocyst/blastocyst.py
+++ src/models/Blastocyst/blastocyst.py
@@ -406,42 +406,42 @@ if __name__ == "__main__":
         "vsg2": 1,  # Maximum rate of GATA6 synthesis caused by its auto-activation
         "vsn1": 0.856,  # Basal rate of NANOG synthesis
         "vsn2": 1,  # Maximum rate of NANOG synthesis caused by its auto-activation
-        # "vsfr1": 2.8,  # Basal rate of FGFR2
-        # "vsfr2": 2.8,  # Maximum rate of FGFR2 synthesis caused by GATA6 activation
-        # "vex": 0.0,  # Basal rate of FGF4 synthesis
-        # "vsf": 0.6,  # Maximum rate of FGF4 synthesis caused by NANOG activation
-        # "va": 20,  # ERK activation rate
-        # "vin": 3.3,  # ERK inactivation rate
-        # "kdg": 1,  # GATA6 degradation rate
-        # "kdn": 1,  # NANOG degradation rate
-        # "kdfr": 1,  # FGFR2 degradation rate
-        # "kdf": 0.09,  # FGF4 degradation rate
-        # "Kag1": 0.28,  # Threshold constant for the activation of GATA6 synthesis by ERK
-        # "Kag2": 0.55,  # Threshold constant for GATA6 auto-activation
-        # "Kan": 0.55,  # Threshold constant for NANOG auto-activation
-        # "Kafr": 0.5,  # Threshold constant for the activation of FGFR2 synthesis by GATA6
-        # "Kaf": 5,  # Threshold constant for the activation of FGF4 synthesis by NANOG
-        # "Kig": 2,  # Threshold constant for the inhibition of GATA6 synthesis by NANOG
-        # "Kin1": 0.28,  # Threshold constant for the inhibition of NANOG synthesis by ERK
-        # "Kin2": 2,  # Threshold constant for the inhibition of NANOG synthesis by GATA6
-        # "Kifr": 0.5,  # Threshold constant for the inhibition of FGFR2 synthesis by NANOG
-        # "Ka": 0.7,  # Michaelis constant for activation of the ERK pathway
-        # "Ki": 0.7,  # Michaelis constant for inactivation of the ERK pathway
-        # "Kd": 2,  # Michaelis constant for activation of the ERK pathway by FGF4
-        # "r": 3,  # Hill coefficient for the activation of GATA6 synthesis by ERK
-        # "s": 4,  # Hill coefficient for GATA6 auto-activation
-        # "q": 4,  # Hill coefficient for the inhibition of GATA6 synthesis by NANOG
-        # "u": 3,  # Hill coefficient for the inhibition of NANOG synthesis by ERK
-        # "v": 4,  # Hill coefficient for NANOG auto-activation
-        # "w": 4,  # Hill coefficient for the inhibition of NANOG synthesis by GATA6
-        # "z": 4,  # Hill coefficient for the activation of FGF4 synthesis by NANOG
-        # "k": 0.32,
-        # "b": 2,
-        # "a": 1,
-        # "th": 0.5,
-        # "i": 1.5,
-        # "d": 0.4,
-        # "n": 4,
+        "vsfr1": 2.8,  # Basal rate of FGFR2
+        "vsfr2": 2.8,  # Maximum rate of FGFR2 synthesis caused by GATA6 activation
+        "vex": 0.0,  # Basal rate of FGF4 synthesis
+        "vsf": 0.6,  # Maximum rate of FGF4 synthesis caused by NANOG activation
+        "va": 20,  # ERK activation rate
+        "vin": 3.3,  # ERK inactivation rate
+        "kdg": 1,  # GATA6 degradation rate
+        "kdn": 1,  # NANOG degradation rate
+        "kdfr": 1,  # FGFR2 degradation rate
+        "kdf": 0.09,  # FGF4 degradation rate
+        "Kag1": 0.28,  # Threshold constant for the activation of GATA6 synthesis by ERK
+        "Kag2": 0.55,  # Threshold constant for GATA6 auto-activation
+        "Kan": 0.55,  # Threshold constant for NANOG auto-activation
+        "Kafr": 0.5,  # Threshold constant for the activation of FGFR2 synthesis by GATA6
+        "Kaf": 5,  # Threshold constant for the activation of FGF4 synthesis by NANOG
+        "Kig": 2,  # Threshold constant for the inhibition of GATA6 synthesis by NANOG
+        "Kin1": 0.28,  # Threshold constant for the inhibition of NANOG synthesis by ERK
+        "Kin2": 2,  # Threshold constant for the inhibition of NANOG synthesis by GATA6
+        "Kifr": 0.5,  # Threshold constant for the inhibition of FGFR2 synthesis by NANOG
+        "Ka": 0.7,  # Michaelis constant for activation of the ERK pathway
+        "Ki": 0.7,  # Michaelis constant for inactivation of the ERK pathway
+        "Kd": 2,  # Michaelis constant for activation of the ERK pathway by FGF4
+        "r": 3,  # Hill coefficient for the activation of GATA6 synthesis by ERK
+        "s": 4,  # Hill coefficient for GATA6 auto-activation
+        "q": 4,  # Hill coefficient for the inhibition of GATA6 synthesis by NANOG
+        "u": 3,  # Hill coefficient for the inhibition of NANOG synthesis by ERK
+        "v": 4,  # Hill coefficient for NANOG auto-activation
+        "w": 4,  # Hill coefficient for the inhibition of NANOG synthesis by GATA6
+        "z": 4,  # Hill coefficient for the activation of FGF4 synthesis by NANOG
+        "k": 0.32,
+        "b": 2,
+        "a": 1,
+        "th": 0.5,
+        "i": 1.5,
+        "d": 0.4,
+        "n": 4,
         # "basal": 0.0,
         # "same": 0.0,
         # "dcm": 0.0,
@@ -536,8 +536,8 @@ if __name__ == "__main__":
     experiment_id = datetime.now().strftime("%Y%m%d_%H%M%S")
     experiment_base_path = experiment_base_path + "/" + experiment_id
 
-    n_samples = 3
-    std_sample = 0.0
+    n_samples = 5
+    std_sample = 0.1
     original_params = params.copy()
     metrics_data = []
 
@@ -549,6 +549,8 @@ if __name__ == "__main__":
         sampled_values = np.random.normal(
             loc=original_params[key], scale=std_sample, size=n_samples
         )
+        # make sure value is not negative
+        sampled_values = np.maximum(sampled_values, 0)
 
         for value in sampled_values:
             current_params = original_params.copy()
